d12847f28877f400e07f2675957138a85c73f52a
Ismaeel <maeelali92@gmail.com>
2016-01-05 04:18:19 +0000

    initial attempt to make grid of 16x16 square divs

---
+++ js/etchASketch.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+var tile = $('<div class="tile"></div>);
+for (var i = 0; i < 16; i++)
+  {
+    $('#wrapper').append(tile);
+  }
+  
+  $('.tile').css('border-color', '1px solid #967');
+  $('.tile').css('width', '25px');
+  $('.tile').css('height', '25px');
+  $('.tile').css('display', 'inline-block');
+  
+});
