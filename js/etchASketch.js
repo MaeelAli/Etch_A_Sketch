be31e2534082090efb6cffef95e1ad74228e095f
Ismaeel <maeelali92@gmail.com>
2016-01-06 22:26:33 -0500

    added Clear button and its functionality to prompt user for size of grid and keep last selected sketch behaviour

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,7 +1,6 @@
 //hardcoded for now
-  var width = 16;
-  var height = 16;
   var pixels = 960;
+  var currentDimension = 0;
 
 $(document).ready(function() {
 
@@ -12,19 +11,18 @@ $(document).ready(function() {
 function initGrid() {
 
    //displays initial 16x16 grid
-  displayGrid(width, height);
+  var dimension = 16;
+  displayGrid(dimension);
 
   //draws as you mouse over (like a pen)
   defaultSketchBehaviour();
 }
 
-function displayGrid(width, height) {
-
-  for (var i = 0; i < height; i++)
+function displayGrid(dimension) {
+  for (var i = 0; i < dimension; i++)
   {
-    for (var j = 0; j < width; j++)
+    for (var j = 0; j < dimension; j++)
     {
-      //var tile = $('<div class="tile" id="' + j + '"></div>');
       var tile = $('<div class="tile"></div>');
       $('.tile-container').append(tile);
     }
@@ -32,8 +30,10 @@ function displayGrid(width, height) {
     $('.tile-container').append(tileRow);
   }
   
-  $('.tile').css('width', pixels / width - 2);
-  $('.tile').css('height', pixels / height - 2);
+  $('.tile').css('width', pixels / dimension - 2);
+  $('.tile').css('height', pixels / dimension - 2);
+  
+  currentDimension = dimension;
 
 }
 
@@ -64,4 +64,38 @@ function fadeBehaviour(){
     tile.css('background-color', 'red');
     tile.fadeTo("slow", 1);
   }); 
+}
+
+function clearGrid(){
+  var dimension = promptGridSize();
+
+  if (dimension > 0)
+  {
+    $('.tile').remove();
+    displayGrid(dimension);
+  }
+
+}
+
+function promptGridSize(){
+  var sizeInput = prompt("Please enter how many squares per side you want your grid to be (1 < x < 128)", currentDimension);
+
+  var dimension = parseInt(sizeInput);
+  console.log(dimension);
+  console.log(isNaN(dimension));
+
+  //user clicked cancel on prompt
+  if(sizeInput === null && typeof sizeInput === "object")
+  {
+    return -1;
+  }
+
+  //user entered number < 1 or > 128 or not a number
+  if (isNaN(dimension) || dimension < 1 || dimension > 128)
+  {
+    alert("Please enter a valid number from 1 to 128");
+    return -1;
+  }
+
+  return dimension;
 }
